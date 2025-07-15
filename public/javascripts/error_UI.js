67887eaedde421f1afc726713f1559ed0ed32c47
Agustin Mora <103913873+agusx64@users.noreply.github.com>
2025-07-15 14:59:51 -0600

    Remove outdated JavaScript files related to ingredient selection and user settings, and replace the recipe registration view with a new HTML file. Update layout CSS to adjust z-index for better UI layering.

--- public/javascripts/error_UI.js
+++
@@ -1,7 +0,0 @@
-let reload_page = document.getElementById('refresh');
-
-reload_page.addEventListener('click', function(){
-
-    window.location.href = '/';
-
-});
--- public/javascripts/success_UI.js
+++
@@ -1,5 +0,0 @@
-let sucess_button = document.getElementById('sucess_button');
-
-sucess_button.addEventListener('click', function() {
-    window.location.href = '/login';
-});
