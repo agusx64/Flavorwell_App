5cd7a9d090a0912c5be142ee9baf013d4cf12544
Agustin Mora <103913873+agusx64@users.noreply.github.com>
2025-07-28 17:08:52 -0600

    Refactor code structure for improved readability and maintainability

--- public/javascripts/started_UI.js
+++ public/javascripts/started_UI.js
@@ -1,7 +1,1 @@
-var RenderMenuButton = document.getElementById('started_render_button');
-
-RenderMenuButton.addEventListener('click', function() {
-
-    window.open('/html/login.html', '_self');
-
-});
+// Redireccionar al login
