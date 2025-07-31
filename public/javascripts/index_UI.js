330621ac275e1546c5866f077b63d1b5377760fc
Agustin Mora <103913873+agusx64@users.noreply.github.com>
2025-07-30 18:12:12 -0600

    fix: remove unused navigation bar configuration code from index_UI.js

--- public/javascripts/index_UI.js
+++ public/javascripts/index_UI.js
@@ -3,20 +3,3 @@ window.onload = function () {
         window.open('./html/started.html', '_self');
     }, 3000);
 };
-
-document.addEventListener('DOMContentLoaded', async () => {
-    if (window.Capacitor?.isNativePlatform?.()) {
-        const { CapacitorNavigationBar } = window.Capacitor.Plugins;
-
-        try {
-            await CapacitorNavigationBar.setNavigationBarColor({
-                color: '#F7B32B',
-                darkButtons: true
-            });
-        } catch (err) {
-            console.warn('No se pudo cambiar la navigation bar:', err);
-        }
-    } else {
-        console.log('No es entorno nativo. Ignorando configuración de navigation bar.');
-    }
-});
