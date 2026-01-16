f056726788d4c8c0ef1ca1492e169db1f699dd8c
Agustin Mora <103913873+agusx64@users.noreply.github.com>
2026-01-16 11:29:44 -0600

    feat: Update API base URL to production environment

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
-    API_BASE_URL: "http://localhost:3000",
-    // API_BASE_URL: "http://localhost:3000"
+    // API_BASE_URL: "http://localhost:3000",
+    API_BASE_URL: "https://flavorwell-production.up.railway.app"
 
 };
 
